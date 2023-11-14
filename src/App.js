b78eb5e6d337bd56257ec962e85d09b5c38c97c6
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2023-11-14 13:32:03 +0000

    ProfilePage created: added ProfileGraphic(SkillsCard and SkillsChart)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavbarMain from './Components/Navbar/Navbar.js';
 import LoginForm from './Components/Forms/LoginPage.js';
 import RegistrationForm from './Components/Forms/RegistrationPage.js';
+import ProfilePage from './Pages/ProfilePage.js';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/registration" element={<RegistrationForm />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
     </>
---
+++ src/Pages/ProfilePage.js
@@ -0,0 +1,8 @@
+import ProfileGraphic from "../Components/ProfileGraphic/ProfileGraphic.js";
+export default function ProfilePage(){
+    return(
+        <div style={{backgroundColor:'#C3CCED'}}>
+          <ProfileGraphic/>
+        </div>
+    );
+}
