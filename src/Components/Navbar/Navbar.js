f5188ff0851b0bb141409713d826d4b26331726a
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2023-11-14 13:34:41 +0000

    Updated Navbar(to design version)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,32 +4,37 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import './Navbar.css';
+import UserIcon from '../../Assets/Images/UserIcon.svg'
 export default function NavbarMain(){
 return(
-<Navbar expand="lg" className="bg-body-tertiary">
+  <div className='navbar_main'>
+<Navbar expand="lg" style={{height:'auto'}} className="bg-body-tertiary navbar text-center">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '120px' }}
+            className="me-auto my-2 my-lg-0 "
+            style={{ width:'100%' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
+            <Nav.Link className='navbar_link' href="#action1">Page 1</Nav.Link>
+            <Nav.Link className='navbar_link' href="#action2">Link</Nav.Link>
 
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/registration">Registration</Nav.Link>
+            <Nav.Link className='navbar_link' href="/login">Login</Nav.Link>
+            <Nav.Link className='navbar_link' href="/registration">Registration</Nav.Link>
            
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+       
           </Nav>
          
         </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+        <Nav.Link className='navbar_link_end' href="/profile" >
+              <img style={{width:'45px'}} src={UserIcon}/>
+            </Nav.Link>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
 );
 }
