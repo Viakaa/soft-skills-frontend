2cddcccf2eec9b1d0cdd47350ea241b34444fcac
Frightness <artur.chopyk.150@gmail.com>
2023-11-20 02:11:05 +0200

    TestCards & InformationCards
    the components TestCards and InformationCards have been added to the ProfilePage

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,8 +1,13 @@
 import ProfileGraphic from "../Components/ProfileGraphic/ProfileGraphic.js";
+import TestCards from "../Components/TestCards/TestCards.js";
+import InformationCards from "../Components/InformationCards/InformationCards.js";
+
 export default function ProfilePage(){
     return(
         <div style={{backgroundColor:'#C3CCED'}}>
-          <ProfileGraphic/>
+            <ProfileGraphic />
+            <TestCards />
+            <InformationCards />
         </div>
     );
 }
