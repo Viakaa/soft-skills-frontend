d9a80dffa0ff2299d15c8a9457ea8dee888b6a2f
Frightness <artur.chopyk.150@gmail.com>
2023-12-08 20:31:58 +0200

    Login_auth
    making a post request when clicking on the login button

--- src/Components/Forms/LoginPage.js
+++ src/Components/Forms/LoginPage.js
@@ -2,16 +2,52 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://ec2-34-239-91-8.compute-1.amazonaws.com/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      // Обробка відповіді з сервера, наприклад, перевірка статусу
+      if (response.ok) {
+        console.log('Successful auth!');
+      } else {
+        console.error('Error');
+      }
+    } catch (error) {
+      console.error('Помилка під час відправки запиту', error);
+    }
+  };
+
   return (
-    <Form
-        style={{ maxWidth: '250px' }}
-    >
+    <Form style={{ maxWidth: '250px' }} onSubmit={handleSubmit}>
       <Form.Group>
         <Form.Label>Email address</Form.Label>
         <Form.Control
           placeholder="Email"
           name="email"
           type="email"
+          value={formData.email}
+          onChange={handleChange}
         />
       </Form.Group>
 
@@ -21,6 +57,8 @@ function LoginForm() {
           placeholder="Password"
           name="password"
           type="password"
+          value={formData.password}
+          onChange={handleChange}
         />
       </Form.Group>
 
