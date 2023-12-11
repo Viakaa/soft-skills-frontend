5ee1dbbf8e0080b44073a092760643dbc71b7876
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2023-12-11 12:58:11 +0000

    added Authentification Service

---
+++ src/Services/authService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "http://ec2-34-239-91-8.compute-1.amazonaws.com/auth/";
+
+
+const register = async (formData) => {
+  try {
+    const response = await axios.post(API_URL + "signup", formData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const login = async (formData) => {
+  try {
+
+  const response = await axios.post(API_URL + "signin", formData);
+  return response.data;
+} catch (error) {
+  console.error(error);
+  throw error;
+}
+};
+
+export default {
+  register,
+  login
+};
+
+
+
