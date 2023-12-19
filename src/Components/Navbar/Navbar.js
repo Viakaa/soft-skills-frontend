9cfbf155d73d4b759ae525b60750120fd4299f10
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2023-12-19 18:47:00 +0000

    Changed accessibility to menu links, checking if user is logged in

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Navbar.css';
 import UserIcon from '../../Assets/Images/UserIcon.svg'
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../Redux/Actions/userActions'; 
 export default function NavbarMain(){
+  const dispatch = useDispatch();
+  const { userData, userInfo } = useSelector((state) => state.auth);
+ // const isLoggedIn = userData || userInfo;
+
+  const authToken = localStorage.getItem('authToken');
+const isLoggedIn = authToken !== null;
+  
+  const handleLogout = () => {
+    dispatch(logout());
+  };
 return(
   <div className='navbar_main'>
 <Navbar expand="lg" style={{height:'auto'}} className="bg-body-tertiary navbar text-center">
@@ -21,17 +33,28 @@ return(
             <Nav.Link className='navbar_link' href="/main">Main</Nav.Link>
             <Nav.Link className='navbar_link' href="/test_constructor">Constructor</Nav.Link>
 
-            <Nav.Link className='navbar_link' href="/login">Login</Nav.Link>
-            <Nav.Link className='navbar_link' href="/registration">Registration</Nav.Link>
+            {!isLoggedIn && (
+                <>
+                  <Nav.Link className='navbar_link' href="/login">Login</Nav.Link>
+                  <Nav.Link className='navbar_link' href="/registration">Registration</Nav.Link>
+                </>
+              )}
            
        
           </Nav>
          
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
-        <Nav.Link className='navbar_link_end' href="/profile" >
-              <img style={{width:'45px'}} src={UserIcon}/>
-            </Nav.Link>
+        {isLoggedIn ? (
+          <div>
+          <Nav.Link className='navbar_link_end' href="/profile">
+          <img style={{ width: '45px' }} src={UserIcon} alt="User Icon" />
+        </Nav.Link>
+              <Nav.Link  variant="navbar_link_end " onClick={handleLogout}>Logout</Nav.Link>
+              </div>
+            ) : (
+              <></>
+            )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
