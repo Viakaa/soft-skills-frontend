89459ae0539bc86f54e37c2d27de83476356ceae
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2024-02-14 21:46:17 +0000

    added check if email exists

--- src/Redux/Actions/userActions.js
+++ src/Redux/Actions/userActions.js
@@ -1,15 +1,35 @@
 
 import axios from "axios";
 
-export const registerUser = (formData) => async (dispatch) => {
-    try {
-      const response = await axios.post("http://ec2-34-239-91-8.compute-1.amazonaws.com/auth/signup", formData);
-      dispatch({ type: 'REGISTER_SUCCESS', payload: response.data });
-      return response.data;
-    } catch (error) {
-      dispatch({ type: 'REGISTER_FAIL', payload: error });
+const getAllUsers = async () => {
+  try {
+      const response = await axios.get("http://ec2-34-239-91-8.compute-1.amazonaws.com/users");
+      return response.data; //return users data
+  } catch (error) {
+      console.error("Error retrieving users:", error);
       throw error;
-    }
+  }
+};
+
+//check if email exists
+const emailExists = (users, email) => {
+  return users.some(user => user.email.toLowerCase() === email.toLowerCase());
+};
+
+export const registerUser = (formData) => async (dispatch) => {
+  try {
+   /* const users = await getAllUsers();
+    if (emailExists(users, formData.email)) {
+      throw new Error("Email already exists. Please use a different email.");
+    }*/
+
+    const response = await axios.post("http://ec2-34-239-91-8.compute-1.amazonaws.com/auth/signup", formData);
+    dispatch({ type: 'REGISTER_SUCCESS', payload: response.data });
+    return response.data;
+  } catch (error) {
+    dispatch({ type: 'REGISTER_FAIL', payload: error.message || error.response.data.message });
+    throw error;
+  }
 };
 
 export const getUserInfo = (userId, token) => async (dispatch) => {
