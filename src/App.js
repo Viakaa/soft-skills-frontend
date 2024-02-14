8aadebe1dd88e85d10204362a8eec3c3d74c3dd2
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2024-02-14 21:43:51 +0000

    Fixed some routes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavbarMain from './Components/Navbar/Navbar.js';
-import LoginForm from './Components/Forms/LoginPage.js';
-import RegistrationForm from './Components/Forms/RegistrationPage.js';
-import MainPage from './Pages/MainPage.js';
-import ProfilePage from './Pages/ProfilePage.js';
-import PrivateRoute from './PrivateRoute';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarMain from "./Components/Navbar/Navbar.js";
+import LoginForm from "./Components/Forms/LoginPage.js";
+import RegistrationForm from "./Components/Forms/RegistrationPage.js";
+import MainPage from "./Pages/MainPage.js";
+import ProfilePage from "./Pages/ProfilePage.js";
+import PrivateRoute from "./PrivateRoute";
 
-import BelbinTest from "./Components/BelbinTest/BelbinTest.js"
-import DNDconstructor from './Components/DNDconstructor/DNDconstructor';
-import DNDcons from './Components/DNDconstructor/DNDcons1';
+import BelbinTest from "./Components/BelbinTest/BelbinTest.js";
+import DNDconstructor from "./Components/DNDconstructor/DNDconstructor";
+import DNDcons from "./Components/DNDconstructor/DNDcons1";
 
-import AdminPage from './Pages/AdminPage';
+import AdminPage from "./Pages/AdminPage";
 import TestPage from "./Pages/TestPage";
 
 function App() {
   return (
     <>
-     <NavbarMain />
-     
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/registration" element={<RegistrationForm />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/adminpanel" element={<AdminPage />} />
+      <NavbarMain />
 
-        <Route path="/profile" element={
-          <PrivateRoute>
-            <ProfilePage />
-          </PrivateRoute>
-        } />
-        <Route path="/test_constructor" element={<DNDconstructor />} />
-        <Route path="/test_constructor1" element={<DNDcons />} />
-        <Route path="/test/:id" element={<TestPage />} />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/registration" element={<RegistrationForm />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/adminpanel" element={<AdminPage />} />
 
-        <Route path="/belbin" element={<BelbinTest />} />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/test_constructor1" element={<DNDconstructor />} />
+          <Route
+            path="/test_constructor"
+            element={
+              <PrivateRoute>
+                <DNDcons />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/test/:id" element={<TestPage />} />
 
-      </Routes>
-    </Router>
+          <Route path="/belbin" element={<BelbinTest />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,63 +1,93 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import './Navbar.css';
-import UserIcon from '../../Assets/Images/UserIcon.svg'
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../../Redux/Actions/userActions'; 
-export default function NavbarMain(){
-  const dispatch = useDispatch();
-  const { userData, userInfo } = useSelector((state) => state.auth);
- // const isLoggedIn = userData || userInfo;
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./Navbar.css";
+import UserIcon from "../../Assets/Images/UserIcon.svg";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../Redux/Actions/userActions";
+import { getUserInfo } from "../../Redux/Actions/userActions.js";
+import React, { useState, useEffect } from "react";
 
-  const authToken = localStorage.getItem('authToken');
-const isLoggedIn = authToken !== null;
-  
+export default function NavbarMain() {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.auth);
+  // const isLoggedIn = userData || userInfo;
+  const isAdmin = userInfo?.role === 'ADMIN';
+
+
+  const authToken = localStorage.getItem("authToken");
+  const isLoggedIn = authToken !== null;
+
   const handleLogout = () => {
     dispatch(logout());
   };
-return(
-  <div className='navbar_main'>
-<Navbar expand="lg" style={{height:'auto'}} className="bg-body-tertiary navbar text-center">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0 "
-            style={{ width:'100%' }}
-            navbarScroll
-          >
-            <Nav.Link className='navbar_link' href="/main">Main</Nav.Link>
-            <Nav.Link className='navbar_link' href="/test_constructor">Constructor</Nav.Link>
+  useEffect(() => {
+    if(isLoggedIn) {
+    dispatch(getUserInfo());
+    }
+  }, [dispatch]);
 
-            {!isLoggedIn && (
+  return (
+    <div className="navbar_main">
+      <Navbar
+        expand="lg"
+        style={{ height: "auto" }}
+        className="bg-body-tertiary navbar text-center"
+      >
+        <Container fluid>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 "
+              style={{ width: "100%" }}
+              navbarScroll
+            >
+              <Nav.Link className="navbar_link" href="/main">
+                Main
+              </Nav.Link>
+              {isAdmin && (
+                <NavDropdown className='navbar_link' title="Admin" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="/adminpanel">Admin Panel</NavDropdown.Item>
+                  <NavDropdown.Item href="/test_constructor">Constructor</NavDropdown.Item>
+
+                </NavDropdown>
+              )}
+
+              {!isLoggedIn && (
                 <>
-                  <Nav.Link className='navbar_link' href="/login">Login</Nav.Link>
-                  <Nav.Link className='navbar_link' href="/registration">Registration</Nav.Link>
+                  <Nav.Link className="navbar_link" href="/login">
+                    Login
+                  </Nav.Link>
+                  <Nav.Link className="navbar_link" href="/registration">
+                    Registration
+                  </Nav.Link>
                 </>
               )}
-           
-       
-          </Nav>
-         
-        </Navbar.Collapse>
-        <Navbar.Collapse className="justify-content-end">
-        {isLoggedIn ? (
-          <div>
-          <Nav.Link className='navbar_link_end' href="/profile">
-          <img style={{ width: '45px' }} src={UserIcon} alt="User Icon" />
-        </Nav.Link>
-              <Nav.Link  variant="navbar_link_end " onClick={handleLogout}>Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            {isLoggedIn ? (
+              <div>
+                <Nav.Link className="navbar_link_end" href="/profile">
+                  <img
+                    style={{ width: "45px" }}
+                    src={UserIcon}
+                    alt="User Icon"
+                  />
+                </Nav.Link>
+                <Nav.Link variant="navbar_link_end " onClick={handleLogout}>
+                  Logout
+                </Nav.Link>
               </div>
             ) : (
               <></>
             )}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-);
+  );
 }
