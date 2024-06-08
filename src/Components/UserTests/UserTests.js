d1897d5aad20c7515c0262a6915a9422b7899a50
dmytr-dmytr123 <58878281+dmytr-dmytr123@users.noreply.github.com>
2024-06-08 15:37:44 +0000

    display results on profile page

--- src/Components/UserTests/UserTests.js
+++ src/Components/UserTests/UserTests.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Carousel } from "react-bootstrap";
 import "./UserTests.css";
-{/* 
+
 export default function UserTests() {
   const [matchedData, setMatchedData] = useState({});
   const userId = localStorage.getItem("userId");
@@ -26,8 +26,7 @@ export default function UserTests() {
       console.log('User Results:', userResults);
 
       //fetch characteristics from test results
-      const characteristicsPromises = userResults.tests.flatMap(test =>
-        test.results.characteristics.map(async (char) => {
+      const characteristicsPromises = userResults.characteristics.map(async (char) => {
           try {
             const charResponse = await axios.get(
               `http://ec2-34-239-91-8.compute-1.amazonaws.com/characteristics/${char.characteristicId}`,
@@ -43,7 +42,7 @@ export default function UserTests() {
             return null; //skip char if failed
           }
         })
-      );
+      
 
       const characteristicsDetails = await Promise.all(characteristicsPromises);
       const validCharacteristicsDetails = characteristicsDetails.filter(char => char !== null);
@@ -70,7 +69,7 @@ export default function UserTests() {
   return (
     <div className="main_wrapper">
       {Object.keys(matchedData).length === 0 ? (
-        <div>No data available</div>
+        <div style={{color:'white'}}>No data available</div>
       ) : (
         Object.keys(matchedData).map((softSkill, index) => (
           <div className="test1" key={index}>
@@ -98,71 +97,13 @@ export default function UserTests() {
     </div>
   );
 }
-*/}
+
+/*
 export default function UserTests() {
       const [matchedData, setMatchedData] = useState({});
       const userId = localStorage.getItem("userId");
       const token = localStorage.getItem("authToken"); 
-      useEffect(() => {
-        fetchUserResults();
-      }, [userId, token]);
-    
-      const fetchUserResults = async () => {
-        try {
-          // get user with results
-          const userResultsResponse = await axios.get(
-            `http://ec2-34-239-91-8.compute-1.amazonaws.com/users/${userId}`,
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          const userResults = userResultsResponse.data;
-          console.log('User Results:', userResults);
-    
-          //fetch characteristics from test results
-          const characteristicsPromises = userResults.tests.flatMap(test =>
-            test.results.characteristics.map(async (char) => {
-              try {
-                const charResponse = await axios.get(
-                  `http://ec2-34-239-91-8.compute-1.amazonaws.com/characteristics/${char.characteristicId}`,
-                  {
-                    headers: {
-                      Authorization: `Bearer ${token}`,
-                    },
-                  }
-                );
-                return { ...char, details: charResponse.data };
-              } catch (error) {
-                console.error(`Error fetching characteristic ID ${char.characteristicId}:`, error);
-                return null; //skip char if failed
-              }
-            })
-          );
-    
-          const characteristicsDetails = await Promise.all(characteristicsPromises);
-          const validCharacteristicsDetails = characteristicsDetails.filter(char => char !== null);
-    
-          //orrganizing data by soft-skill
-          const organizedData = validCharacteristicsDetails.reduce((acc, char) => {
-            const softSkillType = char.details.softSkill.type;
-            if (!acc[softSkillType]) {
-              acc[softSkillType] = [];
-            }
-            acc[softSkillType].push({
-              title: char.details.title,
-              points: char.points,
-            });
-            return acc;
-          }, {});
-    
-          setMatchedData(organizedData);
-        } catch (error) {
-          console.error("Error fetching user results or characteristics:", error);
-        }
-      };
-    
+     
       return (
         <div className="main_wrapper">
        
@@ -203,4 +144,4 @@ export default function UserTests() {
      
         </div>
       );
-    }
+    }*/
