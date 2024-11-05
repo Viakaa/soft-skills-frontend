f871b2a9bd326ec707f9914568b7380d8c8ef1e1
Kekichka <alonadaniluk695@gmail.com>
2024-11-05 15:47:51 +0200

    users actions fix

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-13-61-7-222.compute-1.amazonaws.com/auth/";
+const API_URL = "http://ec2-13-60-83-13.eu-north-1.compute.amazonaws.com/auth/";
 
 
 const register = async (formData) => {
